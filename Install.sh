db16bfed5b4ea0602136f8aa30f96db42b26dc56
mickdec <mickaeldecelle@hotmail.fr>
2018-05-07 15:52:45 +0200

    Adding msf install

--- Install.sh
+++ Install.sh
@@ -8,4 +8,8 @@ git clone https://github.com/Veil-Framework/Veil
 cd Veil/config
 ./setup.sh --force --silent
 cd ../../../src
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+chmod 755 msfinstall && \
+./msfinstall
+rm -r msfinstall
 ./Stratus.sh
