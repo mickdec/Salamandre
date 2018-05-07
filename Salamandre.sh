23d43101592eb2c2fb0e367599324c0cf03976fd
mickdec <mickaeldecelle@hotmail.fr>
2018-05-07 14:40:37 +0200

    adding clone veil

--- Salamandre.sh
+++ Salamandre.sh
@@ -3,7 +3,9 @@ if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root"
    exit 1
 fi
-cd res/Veil/config
+cd res/
+git clone https://github.com/Veil-Framework/Veil
+cd Veil/config
 ./setup.sh --force --silent
 cd ../../../src
 ./Stratus.sh
